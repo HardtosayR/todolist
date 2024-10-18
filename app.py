568f754dc1a620ac848a57351cfdd5c7357edae9
hardtosayr <1011214803@qq.com>
2024-10-18 10:04:01 +0800

    Initial commit of To Do List app

---
+++ app.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+
+class TodoApp:
+    def __init__(self,root) :
+        self.root = root
+        self.root.title("To Do List")
+
+        self.tasks = []
+
+        self.task_entry = tk.Entry(root)
+        self.task_entry.pack()
+
+        self.add_button = tk.Button(root, text="Add Task", command=self.add_task)
+        self.add_button.pack()
+
+        self.task_listbox = tk.Listbox(root)
+        self.task_listbox.pack()
+
+        self.delete_button = tk.Button(root, text="Delete Task", command=self.delete_task)
+        self.delete_button.pack()
+
+    def add_task(self):
+        task = self.task_entry.get()
+        if task:
+            self.tasks.append(task)
+            self.update_task_list()
+            self.task_entry.delete(0, tk.END)
+    
+    def delete_task(self):
+        selected_task_index = self.task_listbox.curselection()
+        if selected_task_index:
+            del self.tasks[selected_task_index[0]]
+            self.update_task_list()
+
+    def update_task_list(self):
+        self.task_listbox.delete(0, tk.END)
+        for task in self.tasks:
+            self.task_listbox.insert(tk.END, task)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TodoApp(root)
+    root.mainloop()
